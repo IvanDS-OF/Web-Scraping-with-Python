26ce33a6323153b3f663679b51faab81263525ed
IvanDS-OF <ivan.durans@outlook.com>
2024-07-22 17:51:51 -0600

    Creating other project - Extracting information from wikipedia

--- ietf_scrape/ietf_scrape/spiders/ietf.py
+++ ietf_scrape/ietf_scrape/spiders/ietf.py
@@ -1,4 +1,5 @@
 import scrapy
+import w3lib.html
 
 
 class IetfSpider(scrapy.Spider):
@@ -6,7 +7,24 @@ class IetfSpider(scrapy.Spider):
     allowed_domains = ["pythonscraping.com"]
     start_urls = ["https://pythonscraping.com/linkedin/ietf.html"]
 
+
+
     def parse(self, response):
+        return {
+            'number': response.xpath('//span[@class="rfc-no"]/text()').get(), 
+            'title': response.xpath('//span[@name="DC.Title"]/@content').get(), 
+            'date': response.xpath('//span[@class="date"]/text()').get(),
+            'description': response.xpath('@meta[@name="DC.Description.Abstract"]/@content').get(), 
+            'author': response.xpath('//meta[@name="DC.Creator"]@ontent').get(), 
+            'company': response.xpath('//span[@class="author_company"]/text()').get(), 
+            'address': response.xpath('//span[@class="address"]/text()').get(), 
+            'text': w3lib.html.remove.tags(response.xpath('//div[@class="text"]').get()), 
+            'headings': response.xpath('//span[@@class="subheading"]/text').get()
+        }
+
+
+
+    def parse0(self, response):
         #To start with the project we can call the title of the page, so we need to go to the Source page
         #and find the  <span class="title">A Standard for the Transmission of IP Datagrams on Avian Carriers</span>
         #It is gonna be in HTML, so, once identified, we have 3 options to obtain this information from the page
@@ -25,3 +43,6 @@ class IetfSpider(scrapy.Spider):
         #This command returns all the values found, we must filter the information although
 
         #pass
+
+
+        
