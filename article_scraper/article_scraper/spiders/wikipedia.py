8f7118d25c57d3fe9cd6b044d4b3dc8e9855735a
IvanDS-OF <ivan.durans@outlook.com>
2024-07-22 19:49:59 -0600

    Ending with Rules and Creawling

--- article_scraper/article_scraper/spiders/wikipedia.py
+++ article_scraper/article_scraper/spiders/wikipedia.py
@@ -1,10 +1,24 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-
-class WikipediaSpider(scrapy.Spider):
+class WikipediaSpider(CrawlSpider):
     name = "wikipedia"
     allowed_domains = ["en.wikipedia.org"]
-    start_urls = ["https://en.wikipedia.org"]
+    start_urls = ["https://en.wikipedia.org/wiki/Kevin_Bacon"]
+
+
+    #Lets to define th object of the rules, The rules are lists
+    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
+    #This means that each time it finds an interla URL it follows that, then it keeps
+    #following and following and finding new interal URLs 
+    #LinkExtractor - Parses the HTML page and finds new Wiki links to visit 
 
     def parse(self, response):
-        pass
+        #Start with defining what we want
+        return {
+            'tittle': response.xpath('//h1/text()').get or response.xpath('//h1/i/text()').get(),
+            'url': response.url, 
+            'last_edited': response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
+        }
+        
